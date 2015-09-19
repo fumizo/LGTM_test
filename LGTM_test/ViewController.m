3c81ab7d8275ae1922fe1689dbd2b07952892468
Ia Kawasaki <ia_0212justin-716cherry@softbank.ne.jp>
2015-09-19 14:47:57 +0900

    first

--- LGTM_test/ViewController.m
+++ LGTM_test/ViewController.m
@@ -14,6 +14,10 @@
 
 @implementation ViewController
 
+-(IBAction)plus{
+    number = number +1;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
